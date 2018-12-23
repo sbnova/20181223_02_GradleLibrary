cbb4a3c47ca55340e22c49f8727369a5e1919e36
test <test1>
2018-12-23 14:12:35 +0900

    글라이드 이미지 출력 성공

--- app/src/main/java/com/cyj/a20181223_02_gradlelibrary/MainActivity.java
+++ app/src/main/java/com/cyj/a20181223_02_gradlelibrary/MainActivity.java
@@ -1,11 +1,13 @@
 package com.cyj.a20181223_02_gradlelibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.ImageView profileImgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +26,13 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        String iuProfileImageURL = "http://www.topstarnews.net/news/photo/201807/450297_101922_452.jpg";
+
+        Glide.with(mContext).load(iuProfileImageURL).into(profileImgView);
     }
 
     @Override
     public void bindViews() {
-
+        this.profileImgView = (ImageView) findViewById(R.id.profileImgView);
     }
 }
